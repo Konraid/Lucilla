f0f85b49dcd0a60ce32470c158e820567691feae
Konrad <k.brandts@arcor.de>
2020-10-01 16:24:54 +0200

    first Hello World Program

--- main.cpp
+++ main.cpp
@@ -3,9 +3,43 @@
 #include <stdio.h>
 #include <CL/cl.hpp>
 
-int main(void)
+int main()
 {
-    cl::Platform platforms;
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
 
-    return 0;
+    auto platform = platforms.front();
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+    std::cout << "Hello WOrld." << devices.size();
+
+
+    auto device = devices.front();
+
+
+    std::ifstream helloWorldFile("hello.cl");
+    std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
+
+    cl::Program::Sources sources( 1, std::make_pair(src.c_str(), src.length() + 1));
+
+    cl::Context context(device);
+    cl::Program program(context, sources);
+
+
+
+    auto err = program.build("-cl-std=CL1.2");
+
+    char buf[16];
+    cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
+    cl::Kernel kernel(program, "HelloWorld", &err);
+    kernel.setArg(0, memBuf);
+
+    cl::CommandQueue queue(context, device);
+    queue.enqueueTask(kernel);
+    queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
+
+    std::cout << "hello";
+    std::cin.get();
+
 }
