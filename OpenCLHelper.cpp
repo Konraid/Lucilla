d34cb69729f7c10d2acf895266e97b9ee0c9bad0
Konrad <k.brandts@arcor.de>
2020-10-01 22:12:07 +0200

    minor clean up

--- OpenCLHelper.cpp
+++ OpenCLHelper.cpp
@@ -11,28 +11,8 @@ cl::Program CreateProgram(const std::string& file) {
 
     auto device = devices.front();
 
-    std::ifstream ifs("HelloWorld.cl");
-    std::string content( (std::istreambuf_iterator<char>(ifs) ),
-                         (std::istreambuf_iterator<char>()    ) );
-
-
-    std::ifstream input(file);
-    std::stringstream sstr;
-
-    while(input >> sstr.rdbuf());
-
-    std::cout << sstr.str() << std::endl;
-
-    std::cout << "FILE: " << "" << std::endl;
-
     std::ifstream fileStream(file);
     std::string src(std::istreambuf_iterator<char>(fileStream), (std::istreambuf_iterator<char>()));
-
-    if(src.empty())
-    {
-        std::cout << "ICH WERDE AUSGEFUEHRT";
-    }
-    std::cout << src.c_str() << std::endl;
 
     cl::Program::Sources sources( 1, std::make_pair(src.c_str(), src.length() + 1));
 
--- main.cpp
+++ main.cpp
@@ -30,7 +30,6 @@ int main()
     }
 
     int buf[16];
-    std::cout << sizeof(buf) << std::endl;
     cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
 
     cl::Kernel kernel(program, "HelloWorld");
@@ -38,27 +37,12 @@ int main()
 
     cl::CommandQueue queue(context, device);
     queue.enqueueTask(kernel);
-    // queue.enqueueReadBuffer(memBuf, 1, 0, sizeof(buf), buf);
-    queue.enqueueReadBuffer(memBuf, 2, 0, sizeof(buf), buf);
+    queue.enqueueReadBuffer(memBuf, 1, 0, sizeof(buf), buf);
 
-    char test[16];
+    for (int i = 0; i<16; i++) {
+        std::cout << buf[i] << std::endl;
+    }
 
-    test[0] = 'A';
-    test[1] = 'B';
-    test[2] = 'C';
-    test[3] = 'D';
-    test[4] = 'E';
-    test[5] = 'F';
-    test[6] = 'G';
-    test[7] = 'H';
-    test[8] = 'I';
-    test[9] = 'j';
-    test[10] = 'K';
-
-    //std::cout << std::endl << std::endl << test << std::endl << std::endl;
-
-
-    std::cout << (buf[1]) << "POPO";
     std::cin.get();
 
 }
