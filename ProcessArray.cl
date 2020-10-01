50320d718ae202964dede3001d72c5f0be798b59
Konrad <k.brandts@arcor.de>
2020-10-02 00:30:33 +0200

    tried some array manipulation

--- ProcessArray.cl
+++ ProcessArray.cl
@@ -1,4 +1,3 @@
-__kernel void ProcessArray(__global int* data, __global int* outData)
-{
-    outData[get_global_id(0)] = data[get]
+__kernel void ProcessArray(__global int* i_data, __global int* o_data) {
+    o_data[ get_global_id(0) ] = i_data[ get_global_id(0) ] * 2;
 }
--- main.cpp
+++ main.cpp
@@ -15,8 +15,7 @@ void printDevicesInfo(std::vector<cl::Device> devices) {
     std::cout << "================================================" << std::endl;
 }
 
-int main()
-{
+void memTest() {
     bool DEBUG = true;
 
     cl::Program program = CreateProgram("../HelloWorld.cl");
@@ -41,6 +40,47 @@ int main()
 
     for (int i = 0; i<16; i++) {
         std::cout << buf[i] << std::endl;
+    }
+
+    std::cin.get();
+}
+
+int main()
+{
+    bool DEBUG = true;
+
+    cl::Program program = CreateProgram("../ProcessArray.cl");
+
+    cl::Context context = program.getInfo<CL_PROGRAM_CONTEXT>();
+    auto devices = context.getInfo<CL_CONTEXT_DEVICES>();
+    auto& device = devices.front();
+
+    if(DEBUG) {
+        printDevicesInfo(devices);
+    }
+
+    std::vector<int> vec(10);
+    std::fill(vec.begin(), vec.end(), 6);
+
+    cl::Buffer inBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(int)
+        * vec.size(), vec.data());
+
+    cl::Buffer outBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(int)
+                                                                                                 * vec.size());
+    cl::Kernel kernel(program, "ProcessArray");
+    kernel.setArg(0, inBuf);
+    kernel.setArg(1, outBuf);
+
+    cl::CommandQueue queue(context, device);
+    //queue.enqueueFillBuffer(inBuf, cl::patt)
+    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(vec.size()));
+    queue.enqueueReadBuffer(outBuf, CL_TRUE, 0, sizeof(int) * vec.size(), vec.data());
+
+    cl::finish();
+
+
+    for (int i = 0; i<16; i++) {
+        std::cout << vec[i] << std::endl;
     }
 
     std::cin.get();
